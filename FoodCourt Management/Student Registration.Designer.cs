4e0ea1150274dea77d9877a72b8cb5763e7221a5
saketh kaparthi <sakethkaparthi@outlook.com>
2016-04-13 14:27:55 +0530

    init commit and student registration

---
+++ FoodCourt Management/Student Registration.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace FoodCourt_Management
+{
+    partial class StudentRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mname = new System.Windows.Forms.TextBox();
+            this.lname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.regno = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.datepicker = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.phno = new System.Windows.Forms.TextBox();
+            this.coursebox = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.address = new System.Windows.Forms.RichTextBox();
+            this.submitbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fname
+            // 
+            this.fname.Location = new System.Drawing.Point(224, 46);
+            this.fname.Name = "fname";
+            this.fname.Size = new System.Drawing.Size(100, 20);
+            this.fname.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // mname
+            // 
+            this.mname.Location = new System.Drawing.Point(330, 46);
+            this.mname.Name = "mname";
+            this.mname.Size = new System.Drawing.Size(100, 20);
+            this.mname.TabIndex = 2;
+            // 
+            // lname
+            // 
+            this.lname.Location = new System.Drawing.Point(436, 46);
+            this.lname.Name = "lname";
+            this.lname.Size = new System.Drawing.Size(100, 20);
+            this.lname.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "First";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(327, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Middle";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(433, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Last";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(81, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Registration Number";
+            // 
+            // regno
+            // 
+            this.regno.Location = new System.Drawing.Point(224, 86);
+            this.regno.Name = "regno";
+            this.regno.Size = new System.Drawing.Size(100, 20);
+            this.regno.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(81, 133);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Date of birth";
+            // 
+            // datepicker
+            // 
+            this.datepicker.Location = new System.Drawing.Point(224, 133);
+            this.datepicker.Name = "datepicker";
+            this.datepicker.Size = new System.Drawing.Size(200, 20);
+            this.datepicker.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(81, 292);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Contact";
+            // 
+            // phno
+            // 
+            this.phno.Location = new System.Drawing.Point(224, 292);
+            this.phno.Name = "phno";
+            this.phno.Size = new System.Drawing.Size(100, 20);
+            this.phno.TabIndex = 12;
+            // 
+            // coursebox
+            // 
+            this.coursebox.FormattingEnabled = true;
+            this.coursebox.Items.AddRange(new object[] {
+            "B.Tech",
+            "M.Tech",
+            "MCA",
+            "B.Arch"});
+            this.coursebox.Location = new System.Drawing.Point(224, 338);
+            this.coursebox.Name = "coursebox";
+            this.coursebox.Size = new System.Drawing.Size(121, 21);
+            this.coursebox.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(81, 346);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Course";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(81, 187);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Address";
+            // 
+            // address
+            // 
+            this.address.Location = new System.Drawing.Point(224, 187);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(200, 75);
+            this.address.TabIndex = 16;
+            this.address.Text = "";
+            // 
+            // submitbutton
+            // 
+            this.submitbutton.Location = new System.Drawing.Point(224, 393);
+            this.submitbutton.Name = "submitbutton";
+            this.submitbutton.Size = new System.Drawing.Size(75, 23);
+            this.submitbutton.TabIndex = 17;
+            this.submitbutton.Text = "Submit";
+            this.submitbutton.UseVisualStyleBackColor = true;
+            this.submitbutton.Click += new System.EventHandler(this.submitbutton_Click);
+            // 
+            // StudentRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 491);
+            this.Controls.Add(this.submitbutton);
+            this.Controls.Add(this.address);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.coursebox);
+            this.Controls.Add(this.phno);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.datepicker);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.regno);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lname);
+            this.Controls.Add(this.mname);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fname);
+            this.Name = "StudentRegistration";
+            this.Text = "StudentRegistration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox mname;
+        private System.Windows.Forms.TextBox lname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox regno;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker datepicker;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox phno;
+        private System.Windows.Forms.ComboBox coursebox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.RichTextBox address;
+        private System.Windows.Forms.Button submitbutton;
+    }
+}
---
+++ FoodCourt Management/Student Registration.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+namespace FoodCourt_Management
+{
+    public partial class StudentRegistration : Form
+    {
+        OracleConnection conn;
+        public StudentRegistration()
+        {
+            InitializeComponent();
+        }
+        public void connect()
+        {
+            string oradb = "Data Source=localhost;User ID=dbsproj;Password=password";
+            conn = new OracleConnection(oradb);
+            conn.Open();
+        }
+        private void submitbutton_Click(object sender, EventArgs e)
+        {
+            connect();
+            string f = fname.Text;
+            string m = mname.Text;
+            string l = lname.Text;
+            string reg = regno.Text;
+            string theDate = datepicker.Value.ToString("dd-MMM-yyyy");
+            string add = address.Text;
+            string phone = phno.Text;
+            int course = coursebox.SelectedIndex;
+            OracleCommand cm = new OracleCommand();
+            cm.Connection = conn;
+            cm.CommandText = "insert into student values('"
+                + reg + "','"
+                + f + "','"
+                + m + "','"
+                + l + "','"
+                + theDate + "',"
+                + course + ",'"
+                + phone + "','"
+                + add + "')";
+            cm.CommandType = CommandType.Text;
+            cm.ExecuteNonQuery();
+            MessageBox.Show("Inserted");
+            conn.Close();
+        }
+    }
+}
